54ef300206b920d5e9ced797bca5b7a74bbff36b
Celine Fassone <celinefassone@MacBook-Air-di-Celine.local>
2023-02-10 14:30:08 +0100

    my game is ready

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 const knight = document.getElementById("knight");
+const box = document.getElementById("box");
 const distance = 2;
 const speed = 1;
 let isMoving = false;
 let whichKeyPressed;
+let isInside = false;
 
 function moveCharacter() {
-  if (whichKeyPressed === "w" && knight.offsetTop > 0) {
+  if (
+    whichKeyPressed === "w" &&
+    knight.offsetTop > 0 &&
+    knight.offsetLeft < window.innerWidth - knight.clientWidth
+  ) {
     knight.style.top = knight.offsetTop - distance + "px";
-  } else if (whichKeyPressed === "s" && knight.offsetTop < window.innerHeight) {
+  }
+
+  if (
+    whichKeyPressed === "s" &&
+    knight.offsetTop < window.innerHeight - knight.clientHeight &&
+    knight.offsetLeft < window.innerWidth - knight.clientWidth
+  ) {
     knight.style.top = knight.offsetTop + distance + "px";
-  } else if (
+  }
+
+  if (
     whichKeyPressed === "d" &&
-    knight.offsetLeft < window.innerWidth - knight.clientWidth / 2
+    knight.offsetLeft < window.innerWidth - knight.clientWidth
   ) {
     knight.style.left = knight.offsetLeft + distance + "px";
-  } else if (whichKeyPressed === "a" && knight.offsetLeft > 0) {
+  }
+
+  if (whichKeyPressed === "a" && knight.offsetLeft > 0) {
     knight.style.left = knight.offsetLeft - distance + "px";
+  }
+
+  insideTheBox();
+}
+
+function insideTheBox() {
+  if (
+    knight.offsetTop >= box.offsetTop &&
+    knight.offsetLeft >= box.offsetLeft &&
+    knight.offsetTop <= box.offsetTop + box.offsetHeight &&
+    knight.offsetLeft <= box.offsetLeft + box.offsetWidth
+  ) {
+    box.style.backgroundColor = "purple";
+  } else {
+    box.style.backgroundColor = "lightgreen";
   }
 }
 
